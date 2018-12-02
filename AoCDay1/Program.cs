f9ecc54307ada19e169c60465454451552f2e685
Timo E <eismar@gmail.com>
2018-12-02 19:24:58 +0100

    added day 1 and 2

---
+++ AoCDay1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 2");
+            Console.WriteLine("Part 1");
+
+            var dataFile = File.ReadAllLines(@"C:\Input\AoC\Day1\data.txt");
+            var dataList = new List<string>(dataFile);
+            int freq = 0;
+            foreach (string s in dataList)
+            {
+                freq += int.Parse(s);
+
+            }
+            Console.WriteLine(freq);
+            Console.WriteLine("Part 2");
+            
+            
+
+            freq = 0;
+            int freqStore = 0;
+            List<int> SeenFreq = new List<int>();
+            bool done = false;
+            while (!done)
+            {
+                for (int i = 0; i < dataList.Count; i++)
+                {
+                    SeenFreq.Add(freq);
+                    freq += int.Parse(dataList[i]);
+                    if (SeenFreq.Contains(freq))
+                    {
+                        Console.WriteLine(freq);
+                        done = true;
+                    }
+                        
+                }
+            }
+            Console.WriteLine(freq);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ AoCDay2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoCDay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 2");
+            Console.WriteLine("Part 1");
+            int twos = 0;
+            int threes = 0;
+            var dataFile = File.ReadAllLines(@"C:\Input\AoC\Day2\data.txt");
+            foreach (string s in dataFile)
+            {
+                var counted = s.GroupBy(x => x)
+                    .ToDictionary(l => l.Key, l => l.Count());
+                if (counted.ContainsValue(2))
+                    twos++;
+                //twos += counted.Count(x => x.Value == 2);
+                if (counted.ContainsValue(3))
+                    threes++;
+                //threes += counted.Count(x => x.Value == 3);
+            }
+            Console.WriteLine("CheckSum: " + twos * threes);
+            Console.WriteLine("Part2");
+            foreach (string s in dataFile)
+            {
+                char[] sa = s.ToCharArray();
+                
+                foreach (string b in dataFile)
+                {
+                    char[] ba = b.ToCharArray();
+
+                    int counter = 0;
+                    for (int i= 0; i < sa.Length;i++)
+                    {
+
+                        if (sa[i] == ba[i])
+                        {
+                            counter++;
+                        }
+                    }
+                    if (counter == 25)
+                    {
+                        Console.WriteLine(s);
+                        Console.WriteLine(b);
+                        Console.WriteLine("---");
+                    }
+                    counter = 0;
+
+                }
+            }
+            Console.ReadLine();
+        }
+
+      
+    }
+}
